8ee3c6456f03d2998b3193c4517a7cdf177cbbe5
devmanagement23 <devmanagement23@gmail.com>
2023-05-11 08:40:57 +0530

    footer change

--- partials/_footer.php
+++ partials/_footer.php
@@ -1,3 +1,4 @@
 <div class="container-fluid bg-dark text-light">
     <p class="text-center mb-0">Copyright DiscussAnything Forums 2023 | All rights reserved</p>
+    <p class="text-center mb-0"><pre>      Youtube      Twitter       Facebook       </pre></p>
 </div>
